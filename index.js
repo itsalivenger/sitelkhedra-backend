0955ee8bae6404550e47530969c6d378dfadbcbb
itsalivenger <alihoussa16@gmail.com>
2021-11-08 16:53:31 +0100

    d

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ mongoose.connect(dbURL,{useNewUrlParser:true})
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 const corsOptions ={
-   origin:'https://wail-bazaar.herokuapp.com', 
+   origin:'https://localhost:3000', 
    credentials:true,
    optionSuccessStatus:200,
 }
