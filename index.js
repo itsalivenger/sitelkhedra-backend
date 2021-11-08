7c9bec713cfdfc21e8bfed8a94fff44c4ab7872f
itsalivenger <alihoussa16@gmail.com>
2021-11-08 15:24:10 +0100

    don

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ mongoose.connect(dbURL,{useNewUrlParser:true})
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 const corsOptions ={
-   origin:'http://localhost:3000/', 
+   origin:'*',
    credentials:true,
    optionSuccessStatus:200,
 }
