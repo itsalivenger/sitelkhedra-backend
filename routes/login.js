4c97cd884a99ad9c6cf32d515d9e1a90c9ce220a
itsalivenger <alihoussa16@gmail.com>
2021-11-11 20:14:29 +0100

    d

--- routes/login.js
+++ routes/login.js
@@ -2,17 +2,17 @@ let router = require('express').Router();
 let bcrypt = require('bcrypt');
 const {User, userSchema} = require("../models/users.js");
 const createToken = require('../jwt/jwtGen.js');
-const authMiddleWare = require('../jwtCheckMiddleWare/jwtCheck.js');
 
 router.post('/', (req, res)=>{
     let { email, password} = req.body;
     User.findOne({ email }).then((found)=>{
-        if(found){
+        let user = found;
+        if(user){
             bcrypt.compare(password, found.password).then((matches)=>{
                 if(matches){
                     res.cookie('jwt', createToken(found._id), {secure: true, sameSite: "none"});
-                    res.cookie('user', 'found', {secure: true, sameSite: "none"});
-                    console.log('user found')
+                    res.cookie('user', user, {secure: true, sameSite: "none"});
+                    console.log('user found');
                     res.status(201).send("Cookie Set");
                 }else{
                     res.status(404).send({errMsg: 'Wrong credentials'});
--- routes/logout.js
+++ routes/logout.js
@@ -2,6 +2,7 @@ let router = require('express').Router();
 
 router.get('/', (req, res)=>{
     res.cookie("jwt", '', { secure: true, sameSite: 'none'});
+    res.cookie("user", '', { secure: true, sameSite: 'none'});
     res.send('Cookie Set');
 });
 
